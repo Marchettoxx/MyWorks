c117078079065cf0fe6dd1c5e05b38ec67134fa1
Marchettoxx <massagrandesmarcos@gmail.com>
2024-10-15 17:58:58 +0200

    add icon for links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { provideHttpClient } from '@angular/common/http';
-import { InfoComponent } from './components/info/info.component';
-import { HomeComponent } from './components/home/home.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { provideHttpClient } from "@angular/common/http";
+import { InfoComponent } from "./components/info/info.component";
+import { HomeComponent } from "./components/home/home.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProjectsComponent,
-    InfoComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [provideHttpClient()],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ProjectsComponent,
+        InfoComponent,
+        HomeComponent,
+        FooterComponent,
+    ],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [provideHttpClient()],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

1e921e5474c114cb43271e1f63bc9fc46e293130
Marchettoxx <massagrandesmarcos@gmail.com>
2024-10-15 11:45:13 +0200

    fix tel prefix and add facebook contact

--- src/app/mocks/mock-info.ts
+++ src/app/mocks/mock-info.ts
@@ -1,25 +1,32 @@
-import { Info } from '../interfaces/info';
+import { Info } from "../interfaces/info";
 
 export const INFOS: Info[] = [
-  {
-    imgSrc: './profile.png',
-    title: 'Linkedin',
-    text: "Hi, I'm Marco Massagrande, a student at the University of Verona.",
-    link: "https://www.linkedin.com/in/marco-massagrande-001940299",
-    lastUpdate: '11/10/2024'
-  },
-  {
-    imgSrc: './giticon.jpg',
-    title: 'Github',
-    text: "Hi, I'm Marco, and here is my GitHub profile.",
-    link: "https://github.com/Marchettoxx",
-    lastUpdate: '10/10/2024'
-  },
-  {
-    imgSrc: './insta.jpg',
-    title: 'Instagram',
-    text: "Hi, I'm Marco, and here is my Instagram profile.",
-    link: "https://www.instagram.com/marcomassagrande/",
-    lastUpdate: '10/10/2024'
-  }
+    {
+        imgSrc: "./profile.png",
+        title: "Linkedin",
+        text: "Hi, I'm Marco Massagrande, a student at the University of Verona.",
+        link: "https://www.linkedin.com/in/marco-massagrande-001940299",
+        lastUpdate: "11/10/2024",
+    },
+    {
+        imgSrc: "./giticon.jpg",
+        title: "Github",
+        text: "Hi, I'm Marco, and here is my GitHub profile.",
+        link: "https://github.com/Marchettoxx",
+        lastUpdate: "10/10/2024",
+    },
+    {
+        imgSrc: "./insta.jpg",
+        title: "Instagram",
+        text: "Hi, I'm Marco, and here is my Instagram profile.",
+        link: "https://www.instagram.com/marcomassagrande/",
+        lastUpdate: "10/10/2024",
+    },
+    {
+        imgSrc: "./facebookicon.jpg",
+        title: "Facebook",
+        text: "Hi, I'm Marco, and here is my Facebook profile.",
+        link: "https://www.facebook.com/profile.php?id=100005541960660",
+        lastUpdate: "10/10/2024",
+    },
 ];
